b7b2a13669e0774eb1a9cf6113f4b0ca371cfe09
Schuyler <sky@krushcom.com>
2020-02-07 12:41:04 -0500

    add default route

--- front-compiler/src/app/app-routing.module.ts
+++ front-compiler/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './user/register/register.component';
 import * as Beverage from './caffeinator/beverage/beverage.namespace';
 
 const routes: Routes = [
+    { path: '', redirectTo: '/beverage/calculator', 'pathMatch': 'full'},
     { path:'login', component: LoginComponent },
     { path:'logout', component: LogoutComponent },
     { path:'beverage/calculator', component: Beverage.Component },
